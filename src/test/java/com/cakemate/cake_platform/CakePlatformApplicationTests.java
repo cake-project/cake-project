2a0f0fbc876a4d8feb0213d3f6adff0ee2763185
oni4444 <tldhs47@naver.com>
2025-07-18 01:59:37 +0900

    build: 빌드 설정 및 환경 설정 추가

---
+++ src/test/java/com/cakemate/cake_platform/CakePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakemate.cake_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
