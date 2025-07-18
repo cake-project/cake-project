6811863322115bd9370d95595e1ead67af6353c8
SKARAIki <132039713+SKARAIki@users.noreply.github.com>
2025-07-18 18:14:00 +0900

    refactor : 2번째 베이스엔티티 및 각 엔티티에 createAt->createdAt으로 수정

--- src/main/java/com/cakemate/cake_platform/common/entity/BaseTimeEntity.java
+++ src/main/java/com/cakemate/cake_platform/common/entity/BaseTimeEntity.java
@@ -21,7 +21,7 @@ public abstract class BaseTimeEntity {
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(nullable = false, updatable = false)
+    @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime modifiedAt;
 }
