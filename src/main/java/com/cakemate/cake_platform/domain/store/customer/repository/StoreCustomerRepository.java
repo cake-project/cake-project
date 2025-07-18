89e2492b47feb00d673d73759d5cbb95e4ad5449
김진권(Spring_7기) <sjh99021800@gmail.com>
2025-07-18 21:24:37 +0900

    remove StoreCustomerRepository 필요없어서 제거

--- src/main/java/com/cakemate/cake_platform/domain/store/customer/repository/StoreCustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cakemate.cake_platform.domain.store.customer.repository;
-
-import com.cakemate.cake_platform.domain.store.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreCustomerRepository extends JpaRepository<Store, Long> {
-}
