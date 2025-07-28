c448c3144bbd5f710ab893e31fa2a603e85ee3de
SKARAIki <132039713+SKARAIki@users.noreply.github.com>
2025-07-28 12:27:47 +0900

    refator: apllication local, prod.yml 파일 수정

--- src/main/java/com/cakemate/cake_platform/common/config/JpaConfig.java
+++ src/main/java/com/cakemate/cake_platform/common/config/JpaConfig.java
@@ -1,5 +1,6 @@
 package com.cakemate.cake_platform.common.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -9,4 +10,7 @@ public class JpaConfig {
     /**
      * Jpa의 Auditing기능(@CreateDate, @LastModifiedDate) 활성화 config
      */
+
+
 }
+
