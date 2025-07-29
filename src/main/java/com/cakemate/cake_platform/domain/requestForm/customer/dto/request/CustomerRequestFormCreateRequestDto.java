c8939e573f9c1768407396efe1ccb9ab996bd690
ioscoh <ssn100407@gmail.com>
2025-07-29 15:18:27 +0900

    [ 의뢰서 경로 레스트하게 수정 완료 ] 및 [ 견적저가 달리지 않은 의뢰서만 삭제 가능하게 완료 ] 및 [의뢰서 등록시 작성자가 나오게 수정 완료] [ 현재 댓글 구분은 널체크로 획인 -> 토큰 처리는 고도화시  ]

--- src/main/java/com/cakemate/cake_platform/domain/requestForm/customer/dto/request/CustomerRequestFormCreateRequestDto.java
+++ src/main/java/com/cakemate/cake_platform/domain/requestForm/customer/dto/request/CustomerRequestFormCreateRequestDto.java
@@ -4,7 +4,9 @@ package com.cakemate.cake_platform.domain.requestForm.customer.dto.request;
 import com.cakemate.cake_platform.domain.auth.entity.Customer;
 import com.cakemate.cake_platform.domain.proposalForm.entity.ProposalForm;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -21,12 +23,18 @@ public class CustomerRequestFormCreateRequestDto {
 
     @NotNull(message = "내용을 입력하세요.")
     private String content;
+
     @NotNull(message = "지역을 입력하세요.")
     private String region;
+
     @NotNull(message = "가격을 입력하세요.")
+    @Positive(message = "희망 가격은 0보다 커야 합니다.")
     private Integer desiredPrice;
+
     private String image;
+
     @NotNull(message = "픽업일을 입력하세요.")
+    @Future(message = "픽업 날짜는 미래 시점이어야 합니다.")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime pickupDate;
 }
--- src/main/java/com/cakemate/cake_platform/domain/requestForm/customer/service/RequestFormCustomerService.java
+++ src/main/java/com/cakemate/cake_platform/domain/requestForm/customer/service/RequestFormCustomerService.java
@@ -15,6 +15,7 @@ import com.cakemate.cake_platform.domain.requestForm.entity.RequestForm;
 import com.cakemate.cake_platform.domain.requestForm.enums.RequestFormStatus;
 import com.cakemate.cake_platform.domain.requestForm.exception.RequestFormAccessDeniedException;
 import com.cakemate.cake_platform.domain.requestForm.exception.NotFoundRequestFormException;
+import com.cakemate.cake_platform.domain.requestForm.exception.RequestFormDeletionNotAllowedException;
 import com.cakemate.cake_platform.domain.store.owner.exception.NotFoundCustomerException;
 import org.springframework.http.HttpStatus;
 import com.cakemate.cake_platform.domain.requestForm.repository.RequestFormRepository;
@@ -81,6 +82,8 @@ public class RequestFormCustomerService {
 
         CustomerRequestFormCreateResponseDto requestFormCustomerResponseDto
                 = new CustomerRequestFormCreateResponseDto(
+                        saveRequestForm.getCustomer().getId(),
+                        saveRequestForm.getCustomer().getName(),
                         saveRequestForm.getId(), saveRequestForm.getTitle(),
                 saveRequestForm.getStatus(), saveRequestForm.getCreatedAt(), saveRequestForm.getDesiredPickupDate()
         );
@@ -197,6 +200,12 @@ public class RequestFormCustomerService {
             // 작성자가 아니면 예외 발생
             throw new RequestFormAccessDeniedException("본인이 작성한 의뢰서만 삭제할 수 있습니다.");
         }
+
+        //견적서 존재 여부 확인
+        if (proposalFormRepository.existsByRequestFormIdAndIsDeletedFalse(requestFormId)) {
+            throw new RequestFormDeletionNotAllowedException( "이미 견적서가 달린 의뢰서는 삭제할 수 없습니다.");
+        }
+
         //삭제(소프트 딜리트)
         requestForm.softDelete();
 
