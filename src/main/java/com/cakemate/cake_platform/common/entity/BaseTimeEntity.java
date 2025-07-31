938b3627005740ccc1ce4c3ea1d6d9babb8a2286
SKARAIki <132039713+SKARAIki@users.noreply.github.com>
2025-07-31 18:39:29 +0900

    basetimeEntity 시간수정

--- src/main/java/com/cakemate/cake_platform/common/entity/BaseTimeEntity.java
+++ src/main/java/com/cakemate/cake_platform/common/entity/BaseTimeEntity.java
@@ -1,15 +1,15 @@
 package com.cakemate.cake_platform.common.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -17,9 +17,20 @@ public abstract class BaseTimeEntity {
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime modifiedAt;
+
+    @PreUpdate
+    public void modifiedAt() {
+        this.modifiedAt = LocalDateTime.now();
+    }
+    @PrePersist
+    public void createdAt() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
