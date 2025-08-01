2b5e3167e5854c298617e7ed55b7fd0a6d42ec9f
oni4444 <tldhs47@naver.com>
2025-08-01 15:14:42 +0900

    feat: 점주 -> 의뢰서 상세 조회에 cakeSize, quantity 추가

--- src/main/java/com/cakemate/cake_platform/domain/requestForm/owner/dto/RequestFormDetailOwnerResponseDto.java
+++ src/main/java/com/cakemate/cake_platform/domain/requestForm/owner/dto/RequestFormDetailOwnerResponseDto.java
@@ -2,35 +2,28 @@ package com.cakemate.cake_platform.domain.requestForm.owner.dto;
 
 import com.cakemate.cake_platform.domain.requestForm.enums.RequestFormStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@AllArgsConstructor
 public class RequestFormDetailOwnerResponseDto {
 
     private Long requestFormId;
     private String customerName;
     private String title;
+    private String cakeSize;
+    private int quantity;
     private String region;
     private String content;
     private int desiredPrice;
     private String image;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime pickupDate;
     private RequestFormStatus status;
-    @JsonFormat
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime createdAt;
 
-    public RequestFormDetailOwnerResponseDto(Long requestFormId, String customerName, String title, String region, String content, int desiredPrice, String image, LocalDateTime pickupDate, RequestFormStatus status, LocalDateTime createdAt) {
-        this.requestFormId = requestFormId;
-        this.customerName = customerName;
-        this.title = title;
-        this.region = region;
-        this.content = content;
-        this.desiredPrice = desiredPrice;
-        this.image = image;
-        this.pickupDate = pickupDate;
-        this.status = status;
-        this.createdAt = createdAt;
-    }
 }
