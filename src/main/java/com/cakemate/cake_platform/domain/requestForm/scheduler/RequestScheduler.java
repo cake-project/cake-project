eb3f9afb66646025a81e0a700a025735c916f30f
김진권(Spring_7기) <sjh99021800@gmail.com>
2025-08-07 17:02:59 +0900

    add 의뢰서 스캐쥴러에 예외처리 추가 및 테스트 완료

--- src/main/java/com/cakemate/cake_platform/domain/requestForm/scheduler/RequestScheduler.java
+++ src/main/java/com/cakemate/cake_platform/domain/requestForm/scheduler/RequestScheduler.java
@@ -24,7 +24,7 @@ public class RequestScheduler {
 
     /**
      * 견적이 달리지 않은 REQUESTED 상태의 의뢰서 중,
-     * desiredPickUpDate가 지난 의뢰서를 매일 새벽 1시에 소프트 딜리트 처리
+     * desiredPickUpDate가 지난 의뢰서를 매일 새벽 1시에 소프트 딜리트 처리(현재는 test용으로 1분간격으로 되어있음)
      */
     @Transactional
     @Scheduled(fixedRate = 60000)
@@ -34,8 +34,8 @@ public class RequestScheduler {
 
         //기준 시간: 현재 시각
         LocalDateTime now = LocalDateTime.now();
-
-        //삭제 대상: status = REQUESTED, desiredPickUpDate < now, isDeleted = false
+        log.info("현재 기준 시간 now: {}", now);
+        //삭제 대상: status = REQUESTED, desiredPickupDate < now, isDeleted = false
         List<RequestForm> expiredRequestForms = requestFormRepository
                 .findByStatusAndDesiredPickupDateBeforeAndIsDeletedFalse(RequestFormStatus.REQUESTED, now);
 
@@ -43,7 +43,8 @@ public class RequestScheduler {
 
         for (RequestForm requestForm : expiredRequestForms) {
             try {
-                requestForm.softDelete();
+                log.info("now: {}, desiredPickupDate: {}", LocalDateTime.now(), requestForm.getDesiredPickupDate());
+                requestForm.validateAndSoftDeleteForScheduler();
                 deletedCount++;
                 log.info("자동 소프트 딜리트된 의뢰서 ID: {}", requestForm.getId());
             } catch (Exception e) {
@@ -57,5 +58,7 @@ public class RequestScheduler {
         log.info("의뢰서 자동 소프트 딜리트 스케쥴러 종료 - 종료시간: {}", endTime);
         log.info("총 진행시간: {}초 ({}밀리초)", duration.toSeconds(), duration.toMillis());
         log.info("실제 소프트 딜리트된 의뢰서 수: {}", deletedCount);
+
+
     }
 }
